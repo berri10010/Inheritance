13c6741c5792c007556e8a8db268d81dd61e1546
Berri <53055829+berri10010@users.noreply.github.com>
2020-02-28 08:52:59 -0500

    Create MyWindow.java

---
+++ MyWindow.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class MyWindow extends JFrame{
+	public static void main(String[] args) {
+		new MyWindow();
+	}
+	
+	public MyWindow() {
+		// TODO Auto-generated constructor stub
+		setSize(1920, 1080);
+		setVisible(true);
+		setTitle("My Window");
+	}
+}
