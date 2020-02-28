9bd88433ff2f3cec4d6192437ffd86637f1782b2
Berri <53055829+berri10010@users.noreply.github.com>
2020-02-28 09:00:36 -0500

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,15 @@
+package Game;
+
+public class Player extends GameObject {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Player draw");
+	}
+	
+	public void random() {
+		
+	}
+
+}
